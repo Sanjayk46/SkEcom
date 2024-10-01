b129e8c57034bffb861b028d0c8e93865e2f2a20
Sanjayk46 <138973406+Sanjayk46@users.noreply.github.com>
2024-10-01 10:14:43 +0530

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const AxiosService = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://skecom-1.onrender.com',
   headers: {
     "Content-Type": "application/json",
   },
